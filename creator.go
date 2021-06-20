8eb9a24095b43a26ccdf0cba6a72bc141f6d0a83
paranauerj <joaopaulocostarj@gmail.com>
2021-06-21 00:23:14 +0100

    Adicionado suporte a controllers

--- creator.go
+++ creator.go
@@ -16,6 +16,11 @@ type Middleware struct {
 	Name string
 }
 
+type Controller struct {
+	Path string
+	Name string
+}
+
 func (p *Project) Exists() bool {
 	projPath := path.Join(os.Getenv("GOPATH"), "src", p.Name)
 	_, err := os.Stat(projPath)
@@ -195,12 +200,82 @@ func ` + name + `() gin.HandlerFunc {
 	return middle
 }
 
+func (c *Controller) Exists() bool {
+	folderPath, _ := os.Getwd()
+	controllerPath := path.Join(folderPath, "controllers")
+
+	_, err := os.Stat(controllerPath)
+
+	if err != nil {
+		return false
+	}
+
+	c.Path = controllerPath
+	return true
+}
+
+func NewController(name string) *Controller {
+	control := &Controller{Name: name}
+	if !control.Exists() {
+		panic("Controllers folder not found!")
+	}
+
+	file, _ := os.Create(path.Join("controllers", name+".go"))
+	file.WriteString(`package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+/*
+* Display a listing of the resource.
+ */
+func index` + strings.Title(name) + `s(c *gin.Context) {
+	
+}
+
+/*
+* Store a newly created resource in storage.
+ */
+func store` + strings.Title(name) + `(c *gin.Context) {
+	
+}
+
+/*
+* Display the specified resource.
+ */
+func show` + strings.Title(name) + `(c *gin.Context) {
+	
+}
+
+/*
+* Update the specified resource in storage.
+ */
+func update` + strings.Title(name) + `(c *gin.Context) {
+	
+}
+
+/*
+* Remove the specified resource from storage.
+ */
+func destroy` + strings.Title(name) + `(c *gin.Context) {
+	
+}
+
+
+`)
+
+	return control
+}
+
 func Creator(args []string) {
 	switch strings.ToLower(args[0]) {
 	case "project":
 		NewProject(args[1])
 	case "middleware":
 		NewMiddleware(args[1])
+	case "controller":
+		NewController(args[1])
 	default:
 		panic("Invalid property to create: " + args[1])
 	}
